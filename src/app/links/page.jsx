e65eb67af49e63af54b7ef761753ee6864ec751b
Thomas Aurelius <teg824@hotmail.com>
2024-01-17 07:46:24 -0600

    poolsupdate

--- src/app/links/page.jsx
+++ src/app/links/page.jsx
@@ -11,12 +11,7 @@ const links = () => {
                <p>List of regional pools used by WVWP</p>
             </Link>
          </div>
-         <div className={styles.link}>
-            <Link href="/links/oep">
-               <h1>Oregon Elite Polo</h1>
-               <p>Information for the OEP team</p>
-            </Link>
-         </div>
+       
          <div className={styles.link}>
             <Link href="https://webpoint.usawaterpolo.com/wp15/Intralock/Login.wp">
                <h1>USA Water Polo</h1>
