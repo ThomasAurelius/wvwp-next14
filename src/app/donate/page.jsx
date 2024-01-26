15b288d9da194630fe05248a3eb52726ef57eef3
Thomas Aurelius <teg824@hotmail.com>
2024-01-26 15:16:53 -0600

    working on paypal

--- src/app/donate/page.jsx
+++ src/app/donate/page.jsx
@@ -1,20 +1,16 @@
 'use client'
 import React, { useEffect } from 'react'
 import styles from './donate.module.css'
-import Script from 'next/script'
+import Image from 'next/image'
+
 
 const DonatePage = () => {
-
-   // useEffect(() => {
-   //    window.location.href = 'https://www.paypal.com/donate?hosted_button_id=Z2WY3VJZ2KQ4L'
-   // }, [])
-
  
 
   return (
     <div className={styles.container}>
-      <Script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></Script>
       
+
 
     DonatePage</div>
   )
