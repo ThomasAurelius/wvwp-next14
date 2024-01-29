4fdb86e6ab71a5c3c9eea7abd33e9c523b0cdf75
Thomas Aurelius <teg824@hotmail.com>
2024-01-29 15:56:05 -0600

    styling

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -4,13 +4,13 @@ const PaymentPage = () => {
   return (
     <div className={styles.container}>
       <h1>Payment Page</h1>
-      <div>
+      <div className={styles.card}>
         <h2>Club Dues</h2>
         <button className={styles.button}><a href="https://buy.stripe.com/test_4gw2bQecR0Iq17a4gg">Pay Club Dues</a>
         </button>
       
       </div>
-      <div>
+      <div className={styles.card}>
         <h2>Tournament Dues</h2>
         <button className={styles.button}><a href="https://buy.stripe.com/test_5kA8Ae4Chbn46rueUV">Pay Tournament Fee</a>
         </button>
