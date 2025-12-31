5f57fd294027712d1b95af253401f7d8a5a1304d
Thomas Gooch <teg824@hotmail.com>
2025-12-31 11:12:15 -0600

    payment plans

--- src/app/parent/page.jsx
+++ src/app/parent/page.jsx
@@ -1,42 +1,38 @@
-import React from 'react'
-import styles from './parent.module.css'
-import AddPlayerForm from '@/components/addPlayerForm/addPlayerForm'
-import { getPlayers } from '@/lib/data'
-import PostCard from '@/components/postCard/postCard'
-import { auth } from '@/lib/auth'
-import Link from 'next/link'
-import PayPalButton from '@/components/paypalButton/PaypalButtonMaster'
+import React from "react";
+import styles from "./parent.module.css";
+import AddPlayerForm from "@/components/addPlayerForm/addPlayerForm";
+import { getPlayers } from "@/lib/data";
+import PostCard from "@/components/postCard/postCard";
+import { auth } from "@/lib/auth";
+import Link from "next/link";
+import PayPalButton from "@/components/paypalButton/PaypalButtonMaster";
 
 const ParentPage = async () => {
+	const session = await auth();
+	const user = session.user;
+	const players = await getPlayers();
+	const playersOfparent = players.filter(
+		(player) => player.parentUserId == user?.id
+	);
 
-  const session = await auth();
-  const user = session.user;
-   const players = await getPlayers();
-   const playersOfparent = players.filter(player => player.parentUserId == user?.id)
+	return (
+		<div className={styles.container}>
+			<h1>Player Details</h1>
+			{playersOfparent.map((player) =>
+				session.isAdmin ? (
+					<Link key={player.id} href={`/players/${player._id}`}>
+						<PostCard key={player.id} player={player} />
+					</Link>
+				) : (
+					<>
+						<PostCard key={player.id} player={player} />
+					</>
+				)
+			)}
+			<AddPlayerForm session={{ session }} />
+			<p>Payment plans available on request!</p>
+		</div>
+	);
+};
 
-  return (    
-    <div className={styles.container}>
-      <h1>Player Details</h1>
-      { playersOfparent.map((player) => (
-         (session.isAdmin) ? ( 
-          <Link key={player.id} href={`/players/${player._id}`}>
-            <PostCard key={player.id} player={player} />
-            
-          </Link>
-            ) : (<>
-          <PostCard key={player.id} player={player} />
-          
-          </>
-          
-            )
-          
-            ))
-            
-      }
-      <AddPlayerForm session={{session}} />
-    </div>
-  )
-}
-
-export default ParentPage
-
+export default ParentPage;
