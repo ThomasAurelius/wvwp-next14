75e85f4d4ae96d30d52cb7a80d463a93f5d63cba
Thomas Gooch <teg824@hotmail.com>
2025-12-31 11:16:41 -0600

    2026

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,26 @@
-import Link from "next/link"
-import styles from "./footer.module.css"
+import Link from "next/link";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return (
-    <div className={styles.footer}>
-      <small>© 2024 Willamette Valley Water Polo</small>
-      <div className={styles.container}> 
-        <Link className={styles.link} href="https://www.facebook.com/wv.waterpolo">Facebook</Link>
-        <Link className={styles.link} href="https://instagram.com/wv_aquatics">Instagram</Link>
-      
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={styles.footer}>
+			<small>© 2026 Willamette Valley Water Polo</small>
+			<div className={styles.container}>
+				<Link
+					className={styles.link}
+					href="https://www.facebook.com/wv.waterpolo"
+				>
+					Facebook
+				</Link>
+				<Link
+					className={styles.link}
+					href="https://instagram.com/wv_aquatics"
+				>
+					Instagram
+				</Link>
+			</div>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
